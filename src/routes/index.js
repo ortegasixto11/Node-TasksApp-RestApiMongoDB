114187ef7e8413ab64da2ac8b29fe4b2befcdb27
Sixto Ortega <sixto_miguel@hotmail.com>
2020-04-13 16:32:44 -0400

    Adding routes for the users controllers

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,18 @@
 module.exports = (app) => {
     const tasks = require("../controllers/tasks.js");
+    const users = require("../controllers/users.js");
 
+    // Tasks
     app.get("/api/tasks", tasks.getAll);
     app.get("/api/tasks/:id", tasks.get);
     app.post("/api/tasks", tasks.create);
     app.put("/api/tasks/:id", tasks.update);
     app.delete("/api/tasks/:id", tasks.remove);
+
+    // Users
+    app.get("/api/users", users.getAll);
+    app.get("/api/users/:id", users.get);
+    app.post("/api/users", users.create);
+    app.put("/api/users/:id", users.update);
+    app.delete("/api/users/:id", users.remove);
 };
